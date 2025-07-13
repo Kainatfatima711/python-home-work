ee5b2e5c196054d8802029b3184594838408c965
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-13 20:00:28 +0500

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,10 @@
+name = "Kainat"
+date = 29
+month = "February"
+year = 2012
+now = 2025
+
+age = now - year
+
+print("Hello i am", name , "i was born on" , date , "", month ,"" ,year)
+print("and now i am" , age )
