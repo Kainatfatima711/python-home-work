66ee049d10c0011d85834e36f9c03856f36d50ee
Kainatfatima711 <kainatfetima@gmail.com>
2025-07-20 15:55:05 +0500

    Add files via upload

---
+++ message.py
@@ -0,0 +1,8 @@
+name =input("Enter your name: ")
+
+#Create a congratulation message using string
+
+message ="Congratulations ," + name + "!You did a great job!", "I am proud of you"
+
+
+print(message)
