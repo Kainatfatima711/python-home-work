b83a9263b4eeb640ce5ac9202156468309f41862
Kainatfatima711 <kainatfetima@gmail.com>
2025-08-02 13:01:18 +0500

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,9 @@
+age =int(input("Enter you age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is greater than 20.")
+else:
+    print("Your age is less than 110.")
